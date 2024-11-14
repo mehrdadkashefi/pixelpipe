e0a768df6b5bcb583c22d87ec125b5bb8a6296aa
Mehrdad Kashefi <kashefi.mehrdad@yahoo.com>
2024-11-14 12:29:38 -0500

    Added plots to sorter results. Added motion estimation based on decentralization

--- pipeline.py
+++ pipeline.py
@@ -10,7 +10,8 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
 import spikeinterface.full as si
-
+from matplotlib import gridspec, rcParams
+import pandas as pd
 
 
 # Define a custom argument type for a list of strings
@@ -25,7 +26,7 @@ def parse_args():
     # Get the path of the recording
     parser.add_argument("--f", type=str,
                             help="path to the recording folder")
-    parser.add_argument("--preprocess", type=bool, default=True,
+    parser.add_argument("--preprocess", type=lambda x: bool(strtobool(x)), default=False, nargs="?", const=True,
                             help="If True, preprocess the data") 
     parser.add_argument("--motion", type=list_of_strs, default=[],
                             help="If true, estimate motion")
@@ -35,7 +36,110 @@ def parse_args():
     args = parser.parse_args()
     return args
 
-    
+def plot_sorting_summary(sorter_path):
+    results_dir = Path(sorter_path / 'sorter_output')
+    ops = np.load(results_dir / 'ops.npy', allow_pickle=True).item()
+    camps = pd.read_csv(results_dir / 'cluster_Amplitude.tsv', sep='\t')['Amplitude'].values
+    contam_pct = pd.read_csv(results_dir / 'cluster_ContamPct.tsv', sep='\t')['ContamPct'].values
+    chan_map =  np.load(results_dir / 'channel_map.npy')
+    templates =  np.load(results_dir / 'templates.npy')
+    chan_best = (templates**2).sum(axis=1).argmax(axis=-1)
+    chan_best = chan_map[chan_best]
+    amplitudes = np.load(results_dir / 'amplitudes.npy')
+    st = np.load(results_dir / 'spike_times.npy')
+    clu = np.load(results_dir / 'spike_clusters.npy')
+    firing_rates = np.unique(clu, return_counts=True)[1] * 30000 / st.max()
+    dshift = ops['dshift']
+    cluster_label = pd.read_csv(results_dir / 'cluster_KSLabel.tsv', sep='\t')['KSLabel'].values
+
+
+    fig = plt.figure(figsize=(10,10), dpi=100)
+    grid = gridspec.GridSpec(3, 3, figure=fig, hspace=0.5, wspace=0.5)
+    if dshift is not None:
+        ax = fig.add_subplot(grid[0,0])
+        ax.plot(np.arange(0, ops['Nbatches'])*2, dshift);
+        ax.set_xlabel('time (sec.)')
+        ax.set_ylabel('drift (um)')
+
+    ax = fig.add_subplot(grid[0,1:])
+    t0 = 0 
+    t1 = np.nonzero(st > ops['fs']*5)[0][0]
+    ax.scatter(st[t0:t1]/30000., chan_best[clu[t0:t1]], s=0.5, color='k', alpha=0.25)
+    ax.set_xlim([0, 5])
+    ax.set_ylim([chan_map.max(), 0])
+    ax.set_xlabel('time (sec.)')
+    ax.set_ylabel('channel')
+    ax.set_title('spikes from units')
+
+    ax = fig.add_subplot(grid[1,0])
+    nb=ax.hist(firing_rates, 20, color='gray')
+    ax.set_xlabel('firing rate (Hz)')
+    ax.set_ylabel('# of units')
+
+    ax = fig.add_subplot(grid[1,1])
+    nb=ax.hist(camps, 20, color='gray')
+    ax.set_xlabel('amplitude')
+    ax.set_ylabel('# of units')
+
+    ax = fig.add_subplot(grid[1,2])
+    nb=ax.hist(np.minimum(100, contam_pct), np.arange(0,105,5), color='grey')
+    ax.plot([10, 10], [0, nb[0].max()], 'k--')
+    ax.set_xlabel('% contamination')
+    ax.set_ylabel('# of units')
+    ax.set_title('< 10% = good units'+ '\n' + str(sum(cluster_label == 'good'))+'/'+str(len(cluster_label)))
+
+    for k in range(2):
+        ax = fig.add_subplot(grid[2,k])
+        is_ref = contam_pct<10.
+        ax.scatter(firing_rates[~is_ref], camps[~is_ref], s=3, color='r', label='mua', alpha=0.25)
+        ax.scatter(firing_rates[is_ref], camps[is_ref], s=3, color='b', label='good', alpha=0.25)
+        ax.set_ylabel('amplitude (a.u.)')
+        ax.set_xlabel('firing rate (Hz)')
+        ax.legend()
+        if k==1:
+            ax.set_xscale('log')
+            ax.set_yscale('log')
+            ax.set_title('loglog')
+    fig.savefig(Path.joinpath(results_dir, 'summary.pdf'), format='pdf')
+    plt.close()
+
+    # Plot summary of good and multiunit waveforms
+    probe = ops['probe']
+    # x and y position of probe sites
+    xc, yc = probe['xc'], probe['yc']
+    nc = 16 # number of channels to show
+    good_units = np.nonzero(contam_pct <= 0.1)[0]
+    mua_units = np.nonzero(contam_pct > 0.1)[0]
+
+
+    gstr = ['good', 'mua']
+    for j in range(2):
+        units = good_units if j==0 else mua_units 
+        fig = plt.figure(figsize=(12,3), dpi=150)
+        grid = gridspec.GridSpec(2,20, figure=fig, hspace=0.25, wspace=0.5)
+
+        for k in range(40):
+            wi = units[np.random.randint(len(units))]
+            wv = templates[wi].copy()  
+            cb = chan_best[wi]
+            nsp = (clu==wi).sum()
+            
+            ax = fig.add_subplot(grid[k//20, k%20])
+            n_chan = wv.shape[-1]
+            ic0 = max(0, cb-nc//2)
+            ic1 = min(n_chan, cb+nc//2)
+            wv = wv[:, ic0:ic1]
+            x0, y0 = xc[ic0:ic1], yc[ic0:ic1]
+            amp = 4
+            for ii, (xi,yi) in enumerate(zip(x0,y0)):
+                t = np.arange(-wv.shape[0]//2,wv.shape[0]//2,1,'float32')
+                t /= wv.shape[0] / 20
+                ax.plot(xi + t, yi + wv[:,ii]*amp, lw=0.5, color='k')
+
+            ax.set_title(f'{nsp}', fontsize='small')
+            ax.axis('off')
+        fig.savefig(Path.joinpath(results_dir, 'summary_wave_'+gstr[j]+'.pdf'), format='pdf')
+        plt.close()
 
 
 if __name__ == "__main__":
@@ -73,6 +177,7 @@ if __name__ == "__main__":
         ax.set_ylim(-100, 200)
         sns.despine(trim=True)
         plt.savefig(probe_folder / plot_folder/ 'probe_map.png')
+        plt.close()
 
         # Preprocess the data
         if args.preprocess:
@@ -121,20 +226,20 @@ if __name__ == "__main__":
                 rec = si.read_binary_folder(probe_folder / 'preprocess')
                 print(rec)
 
-        # Estimate motion
-        from spikeinterface.sortingcomponents.peak_detection import detect_peaks
-        from spikeinterface.sortingcomponents.peak_localization import localize_peaks
-
-        if not (probe_folder / 'preprocess').exists():
-            print('No preprocess folder found. Estimating motion from raw data!')
-            rec = si.read_spikeglx(probe_folder, stream_name='imec'+ str(probe_i) +'.ap', load_sync_channel=False)
-        else:
-            print('Found preprocess folder. Estimating motion from preprocessed data!')
-            rec = si.read_binary_folder(probe_folder / 'preprocess')
-            print(rec)
-
         if len(args.motion) != 0:
             print('Following motion estimation methods will be used:', args.motion)
+            # Estimate motion
+            from spikeinterface.sortingcomponents.peak_detection import detect_peaks
+            from spikeinterface.sortingcomponents.peak_localization import localize_peaks
+
+            if not (probe_folder / 'preprocess').exists():
+                print('No preprocess folder found. Run pre-processing on data!')
+                assert False
+            else:
+                print('Found preprocess folder. Estimating motion from preprocessed data!')
+                rec = si.read_binary_folder(probe_folder / 'preprocess')
+                print(rec)
+
             # Estimate motion
             motion_folder = probe_folder / 'motion'
             if not motion_folder.exists():
@@ -169,7 +274,6 @@ if __name__ == "__main__":
             ## Estimate motion using MEDiCINe
             if 'medicine' in args.motion:
                 import medicine
-                
                 print('Estimating motion using medicine method')
                 # Create directory to store MEDiCINe outputs for this recording
                 medicine_output_dir = Path(motion_folder / 'medicine')
@@ -180,65 +284,127 @@ if __name__ == "__main__":
                         peak_amplitudes=peaks['amplitude'],
                         peak_depths=peak_locations['y'],
                         peak_times=peaks['sample_index'] / rec.get_sampling_frequency(),
-                        output_dir=medicine_output_dir,)   
+                        output_dir=medicine_output_dir) 
+
+                    motion = np.load(medicine_output_dir / 'motion.npy')
+                    time_bins = np.load(medicine_output_dir / 'time_bins.npy')
+                    depth_bins = np.load(medicine_output_dir / 'depth_bins.npy')  
+                    # Plotting the motion
+                    plt.figure(figsize=(2,5))
+                    plt.plot(time_bins, motion + depth_bins, color='k')
+                    plt.ylabel('depth (um)')
+                    plt.xlabel('time (sec)')
+                    sns.despine(trim=True)
+                    plt.savefig(medicine_output_dir / 'motion.png')
+                    plt.close()
                 else:
                     print('Found medicine folder already, skipping!')
+            if 'decenter' in args.motion:
+                from spikeinterface.sortingcomponents.motion.motion_estimation import estimate_motion
+                print('Estimating motion using decenteralization method')
+                decenter_output_dir = Path(motion_folder / 'decenter')
+                if not decenter_output_dir.exists():
+                    decenter_output_dir.mkdir(parents=True, exist_ok=True)
+                    motion = estimate_motion(recording=rec,
+                                             peaks=peaks,
+                                             peak_locations=peak_locations,
+                                             method="decentralized",
+                                             progress_bar=True)
+                    np.save(decenter_output_dir / "temporal_bins_s.npy", motion.temporal_bins_s)
+                    np.save(decenter_output_dir / "displacement.npy", motion.displacement)
+                    np.save(decenter_output_dir / "spatial_bins_um.npy", motion.spatial_bins_um)
+                    # Plotting the motion
+                    plt.figure(figsize=(2,5))
+                    plt.plot(motion.temporal_bins_s[0], motion.displacement[0] + motion.spatial_bins_um, 'k')
+                    plt.ylabel('depth (um)')
+                    plt.xlabel('time (sec)')
+                    sns.despine(trim=True)
+                    plt.savefig(decenter_output_dir / 'motion.png')
+                    plt.close()
+                else:
+                    print('Found Decenter folder already, skipping!')
 
         if args.sort:
             from spikeinterface.sorters import run_sorter
             print('Sorting spikes!')
+            ##############################
             # Sort KiloSort4 from raw data
+            ##############################
             if (probe_folder / 'kilosort4').exists():
                 print('kilosort4 folder already exists, Anicha!')
             else:
                 run_sorter(sorter_name='kilosort4', recording=raw_rec, folder= probe_folder / 'kilosort4',
                         nblocks= 3, batch_size= 60000, sig_interp= 20, verbose=True)
-                
-            # Sort KiloSort4 from preprocessed data
-            if (probe_folder / 'kilosort4_pre').exists():
-                print('kilosort4_pre folder already exists, Anicha!')
-            else:
-                rec = si.read_binary_folder(probe_folder / 'preprocess')
-                run_sorter(sorter_name='kilosort4', recording=rec, folder= probe_folder / 'kilosort4_pre',
-                        nblocks= 3, batch_size= 60000, sig_interp= 20, verbose=True)
-                
-            if (probe_folder / 'motion/medicine').exists():
-                from spikeinterface.sortingcomponents.motion.motion_interpolation import InterpolateMotionRecording
-                from spikeinterface.sortingcomponents.motion import motion_utils
-
-                print('Found medicine motion estimation, sorting with it!')
-                # Sort KiloSort4 after motion correction
-                if (probe_folder / 'kilosort4_pre_medicine').exists():
-                    print('kilosort4_pre_medicine folder already exists, Anicha!')
+                plot_sorting_summary(Path(probe_folder / 'kilosort4'))
+            ######################################################################
+            if (probe_folder / 'preprocess').exists():
+                #######################################
+                # Sort KiloSort4 from preprocessed data
+                #######################################
+                if (probe_folder / 'kilosort4_pre').exists():
+                    print('kilosort4_pre folder already exists, Anicha!')
                 else:
                     rec = si.read_binary_folder(probe_folder / 'preprocess')
-                    rec_float = si.astype(recording=rec, dtype="float32")
-                    # Load motion estimated by MEDiCINe
-                    motion = np.load(medicine_output_dir / 'motion.npy')
-                    time_bins = np.load(medicine_output_dir / 'time_bins.npy')
-                    depth_bins = np.load(medicine_output_dir / 'depth_bins.npy')
-                    motion_object = motion_utils.Motion(
-                        displacement=motion,
-                        temporal_bins_s=time_bins,
-                        spatial_bins_um=depth_bins,
-                    )
-                    # Use interpolation to correct for motion estimated by MEDiCINe
-                    recording_motion_corrected = InterpolateMotionRecording(
-                        rec_float,
-                        motion_object,
-                        border_mode='force_extrapolate')
-                    run_sorter(sorter_name='kilosort4', recording=recording_motion_corrected, folder= probe_folder / 'kilosort4_pre_medicine',
-                        nblocks= 3, batch_size= 60000, sig_interp= 20, verbose=True, do_correction= False)
-
-                    
-                    
-
-
-            
-
-            
-
-
-
-
-
+                    run_sorter(sorter_name='kilosort4', recording=rec, folder= probe_folder / 'kilosort4_pre',
+                            nblocks= 3, batch_size= 60000, sig_interp= 20, verbose=True)
+                    plot_sorting_summary(Path(probe_folder / 'kilosort4_pre'))
+                ######################################################################
+                # Sort KiloSort4 from preprocessed data and medicine motion estimation
+                ######################################################################
+                if (probe_folder / 'motion/medicine').exists():
+                    from spikeinterface.sortingcomponents.motion.motion_interpolation import InterpolateMotionRecording
+                    from spikeinterface.sortingcomponents.motion import motion_utils
+                    # Sort KiloSort4 after motion correction
+                    medicine_output_dir = Path(motion_folder / 'medicine')
+                    if (probe_folder / 'kilosort4_pre_medicine').exists():
+                        print('kilosort4_pre_medicine folder already exists, Anicha!')
+                    else:
+                        rec = si.read_binary_folder(probe_folder / 'preprocess')
+                        rec_float = si.astype(recording=rec, dtype="float32")
+                        # Load motion estimated by MEDiCINe
+                        motion = np.load(medicine_output_dir / 'motion.npy')
+                        time_bins = np.load(medicine_output_dir / 'time_bins.npy')
+                        depth_bins = np.load(medicine_output_dir / 'depth_bins.npy')
+                        motion_object = motion_utils.Motion(
+                            displacement=motion,
+                            temporal_bins_s=time_bins,
+                            spatial_bins_um=depth_bins,
+                        )
+                        # Use interpolation to correct for motion estimated by MEDiCINe
+                        recording_motion_corrected = InterpolateMotionRecording(
+                            rec_float,
+                            motion_object,
+                            border_mode='force_extrapolate')
+                        run_sorter(sorter_name='kilosort4', recording=recording_motion_corrected, folder= probe_folder / 'kilosort4_pre_medicine',
+                                    batch_size= 60000, verbose=True, do_correction= False)
+                        plot_sorting_summary(Path(probe_folder / 'kilosort4_pre_medicine'))
+                ##############################################################################
+                # Sort KiloSort4 from preprocessed data and decentralization motion estimation
+                ##############################################################################
+                if (probe_folder / 'motion/decenter').exists():
+                    from spikeinterface.sortingcomponents.motion.motion_interpolation import InterpolateMotionRecording
+                    from spikeinterface.sortingcomponents.motion import motion_utils
+                    decenter_output_dir = Path(motion_folder / 'decenter')
+                    # Sort KiloSort4 after motion correction
+                    if (probe_folder / 'kilosort4_pre_decenter').exists():
+                        print('kilosort4_pre_decenter folder already exists, Anicha!')
+                    else:
+                        rec = si.read_binary_folder(probe_folder / 'preprocess')
+                        rec_float = si.astype(recording=rec, dtype="float32")
+                        # Load motion estimated by decentralization
+                        displacement = np.load(decenter_output_dir / 'displacement.npy')[0]
+                        temporal_bins_s = np.load(decenter_output_dir / 'temporal_bins_s.npy')[0]
+                        spatial_bins_um = np.load(decenter_output_dir / 'spatial_bins_um.npy')
+                        motion_object = motion_utils.Motion(
+                            displacement=displacement,
+                            temporal_bins_s=temporal_bins_s,
+                            spatial_bins_um=spatial_bins_um,
+                        )
+                        # Use interpolation to correct for motion estimated by decentralization
+                        recording_motion_corrected = InterpolateMotionRecording(
+                            rec_float,
+                            motion_object,
+                            border_mode='force_extrapolate')
+                        run_sorter(sorter_name='kilosort4', recording=recording_motion_corrected, folder= probe_folder / 'kilosort4_pre_decenter',
+                                    batch_size= 60000, verbose=True, do_correction= False)
+                        plot_sorting_summary(Path(probe_folder / 'kilosort4_pre_decenter'))
